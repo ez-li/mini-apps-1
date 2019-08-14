74f3c722416aa6e44c5314c7ebb7e9583167161a
ez-li <eileenli.sf@gmail.com>
2019-08-14 16:38:39 -0700

    Implemented Checkout Initialization, Form 1 Sends Data to Server

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,2 +1,73 @@
 
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            showCheckout: true,
+            f1: false,
+            f2: false
+        };
+        this.handleCheckout = this.handleCheckout.bind(this);
+    }
 
+    handleCheckout() {
+        event.preventDefault();
+        this.setState({
+            showCheckout: false,
+            f1: true,
+            f2: false
+        })
+    }
+
+    handleFormOne() {
+        event.preventDefault();
+        this.setState({
+            f1: false,
+            f2: false,
+        })
+    }
+
+    render() {
+        return (
+                <div>
+                    <form method="POST" action="/">
+                        {this.state.showCheckout ? <button id="button" onClick={this.handleCheckout}>Checkout</button> : null}
+                    </form>
+                    {this.state.f1 ? <FormOne/> : null}
+                </div>
+            )
+    }
+}
+
+function FormOne() {
+    return (
+        <form method="POST" action="/formOne">
+            Name
+            <input id="name" type="text" name="name"></input><br></br>
+            Email
+            <input id="email" type="text" name="email"></input><br></br>
+            Password
+            <input id="password" type="password" name="password"></input><br></br>
+            <input type="submit"></input>
+        </form>
+    )
+}
+
+// function FormTwo() {
+//     return (
+//         <form method="POST" action="/formTwo">
+//             Address
+//             <input id="line1" type="text" name="line1"></input><br></br>
+//             <input id="line2" type="text" name="line2"></input><br></br>
+//             City
+//             <input id="city" type="text" name="city"></input><br></br>
+//             State
+//             <input id="state" type="text" name="state"></input><br></br>
+//             Phone Number
+//             <input id="phone" type="text" name="phone"></input><br></br>
+//             <input type="submit"></input>
+//         </form>
+//     )
+// }
+
+ReactDOM.render(<App />, document.getElementById('app'));
--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,2 +1,42 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+var mongoClient = require('mongodb').MongoClient
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(express.static('public'));
+
+var db = mongoClient.connect('mongodb://localhost:27017/test');
+var test = {name:'eileen', email:'e@world.com', password:'world'}
+db.createCollection('users')
+db.users.insertOne(test)
+
+// mongoClient.connect(url, function(err, db) {
+//     // db.createCollection('users', function(err, collection) {
+//     //     var test = {name:'eileen', email:'e@world.com', password:'world'}
+//     //     collection.insert(test);
+//     //     db.close();
+//     // })
+
+//     var test = {name:'eileen', email:'e@world.com', password:'world'}
+//     db.users.insertOne(test)
+// })
 
 
+app.post('/', (req, res) => {
+    // console.log(req);
+    // upon clicking checkout, take user to page F1
+    res.send()
+})
+
+app.post('/formOne', (req, res) => {
+    // console.log(req.body);
+    const input = {name, email, password} = req.body
+    console.log(input)
+
+    res.send()
+})
+
+app.listen(3000)
+console.log("Listening on Port 3000...")
